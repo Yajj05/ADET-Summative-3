84ab14a8cd7378e961f637de3887dc08d6683810
Yajj05 <202211366@fit.edu.ph>
2024-06-29 16:45:29 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,61 +1,20 @@
 <?php
-// functions.php
-
-// Initialize session if not started
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Function to add an anime
-function addAnime($title, $genre, $price_per_day) {
-    $new_id = count($_SESSION['animes']) + 1;
-    $_SESSION['animes'][] = [
-        'id' => $new_id,
-        'title' => $title,
-        'genre' => $genre,
-        'price_per_day' => $price_per_day
-    ];
-}
-
-// Function to get all animes
-function getAnimes() {
-    return $_SESSION['animes'];
-}
-
-// Function to get a single anime by ID
-function getAnimeById($id) {
-    foreach ($_SESSION['animes'] as $anime) {
-        if ($anime['id'] == $id) {
-            return $anime;
-        }
+function addTask($title, $time, $date) {
+    if (!isset($_SESSION['tasks'])) {
+        $_SESSION['tasks'] = [];
     }
-    return null;
+    $_SESSION['tasks'][] = ['title' => $title, 'time' => $time, 'date' => $date];
 }
 
-// Function to edit an anime
-function editAnime($id, $title, $genre, $price_per_day) {
-    foreach ($_SESSION['animes'] as $key => $anime) {
-        if ($anime['id'] == $id) {
-            $_SESSION['animes'][$key] = [
-                'id' => $id,
-                'title' => $title,
-                'genre' => $genre,
-                'price_per_day' => $price_per_day
-            ];
-            break;
-        }
+function editTask($index, $title, $time, $date) {
+    if (isset($_SESSION['tasks'][$index])) {
+        $_SESSION['tasks'][$index] = ['title' => $title, 'time' => $time, 'date' => $date];
     }
 }
 
-// Function to delete an anime
-function deleteAnime($id) {
-    foreach ($_SESSION['animes'] as $key => $anime) {
-        if ($anime['id'] == $id) {
-            unset($_SESSION['animes'][$key]);
-            $_SESSION['animes'] = array_values($_SESSION['animes']); // Re-index array after unsetting
-            break;
-        }
+function deleteTask($index) {
+    if (isset($_SESSION['tasks'][$index])) {
+        array_splice($_SESSION['tasks'], $index, 1);
     }
 }
 ?>
-
